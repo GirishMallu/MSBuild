8d19c13b63596870b42ba0deb94eac536c090e70
unknown <Z003KKVW@AAEINBR131543L.ad001.siemens.net>
2017-06-02 14:19:31 +0530

    add to git

---
+++ JenkinProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JenkinProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 2;
+            int b = 3;
+            int c = a + b;
+            Console.WriteLine("Hello Everyone...!!");
+            Console.WriteLine("This is a verification project and answer is equal to c");
+            Console.ReadLine();
+        }
+    }
+}
